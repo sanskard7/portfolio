65b001e64ef5bee5053301a603a3c614eefd6fbe
sanskard7 <sanskardongare7@gmail.com>
2026-04-27 00:52:19 +0530

    Fix formatting of image modal display style

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-// IMAGE POPUP
 function openImage(src){
-  document.getElementById("imgModal").style.display = "flex";
+  document.getElementById("imgModal").style.display="flex";
   document.getElementById("modalImg").src = src;
 }
 
 function closeImage(){
-  document.getElementById("imgModal").style.display = "none";
+  document.getElementById("imgModal").style.display="none";
 }
